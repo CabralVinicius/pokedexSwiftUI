e438efa97309082600d5107139c63b5bc760f2be
CabralVinicius <cabral.vm@outlook.com>
2025-09-13 20:31:05 -0300

    Organizing the onboardingView screen and adjustments in the color collection.

--- pokedexSwiftUI/pokedexSwiftUI/Onboarding/View/OnboardingView.swift
+++ pokedexSwiftUI/pokedexSwiftUI/Onboarding/View/OnboardingView.swift
@@ -10,35 +10,81 @@ import SwiftUI
 struct OnboardingView: View {
     var body: some View {
         VStack {
-            ZStack{
-                Image("treinador1")
-                    .offset(x: -50)
-                    .background{
-                        Image("sombra")
-                            .offset(x: -65, y: 110)
-                    }
-                Image("treinador2")
-                    .offset(x: 50, y: -10)
-                    .background{
-                        Image("sombra")
-                            .offset(x: 50, y: 115)
-                    }
-            }
-            
-            VStack{
-                Text("Todos os Pokémons em um só Lugar")
-                    .font(Font.custom("Poppins-Medium.ttf", size: 26))
-                    .multilineTextAlignment(.center)
-                    .foregroundStyle(Color("Primary"))
-                
-                Text("Acesse uma vasta lista de Pokémon de todas as gerações já feitas pela Nintendo")
-                    .font(Font.custom("Poppins-Regular.ttf", size: 14))
-                    .multilineTextAlignment(.center)
-                    .foregroundStyle(Color("Secondary"))
-            }
+            trainersImages
+            Spacer().frame(height: 45)
+            titleAndDescription
+            Spacer().frame(height: 24)
+            onboardingProgressView
+            Spacer().frame(height: 45)
+            continueButton
         }
-        .padding()
-
+        .frame(maxWidth: .infinity,
+               maxHeight: .infinity,
+               alignment: .bottom)
+        .padding(.horizontal, 16)
+    }
+    
+    // MARK: - trainersImages
+    var trainersImages: some View {
+        ZStack{
+            Image("treinador1")
+                .offset(x: -50)
+                .background{
+                    Image("sombra")
+                        .offset(x: -65, y: 110)
+                }
+            Image("treinador2")
+                .offset(x: 50, y: -10)
+                .background{
+                    Image("sombra")
+                        .offset(x: 50, y: 115)
+                }
+        }
+    }
+    
+    // MARK: - titleAndDescription
+    var titleAndDescription: some View {
+        VStack(spacing: 16){
+            Text("Todos os Pokémons em um só Lugar")
+                .font(Font.custom("Poppins-Medium", size: 26))
+                .multilineTextAlignment(.center)
+                .foregroundStyle(Color("AppPrimary"))
+            
+            Text("Acesse uma vasta lista de Pokémon de todas as gerações já feitas pela Nintendo")
+                .font(Font.custom("Poppins-Regular", size: 14))
+                .multilineTextAlignment(.center)
+                .foregroundStyle(Color("AppSecondary"))
+        }
+    }
+    
+    // MARK: - onboardingProgressView
+    var onboardingProgressView: some View {
+        HStack{
+            Rectangle()
+                .frame(width: 28, height: 9)
+                .clipShape(.capsule)
+                .foregroundStyle(Color("DarkBlue"))
+            Circle()
+                .frame(width: 9, height: 9)
+                .foregroundStyle(Color(.lightGray))
+        }
+    }
+    
+    // MARK: - continueButton
+    var continueButton: some View {
+        Button(action: {
+            
+        }, label: {
+            Rectangle()
+                .frame(height: 58)
+                .clipShape(.capsule)
+                .foregroundStyle(Color("DarkBlue"))
+                .overlay {
+                    Text("Continuar")
+                        .foregroundStyle(.white)
+                        .font(Font.custom("Poppins-SemiBold", size: 18))
+                }
+        })
     }
 }
 
